c648f02a3beca76472d073b892f02cf108ada534
Pallav <Pallav.18jics202@jietjodhpur.ac.in>
2020-07-05 14:17:23 +0530

    Updates

---
+++ precache-manifest.70340b2f6ffad04477c59896afcd8a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0862b776e94fec3f1c366a9592feb2",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "f13647a6d0e31e042202",
+    "url": "/weather_app/static/css/main.b538af69.chunk.css"
+  },
+  {
+    "revision": "8b2a81a27130adadad74",
+    "url": "/weather_app/static/js/2.921b4117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_app/static/js/2.921b4117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13647a6d0e31e042202",
+    "url": "/weather_app/static/js/main.5835c267.chunk.js"
+  },
+  {
+    "revision": "ccb6375cbb18051ce6b8",
+    "url": "/weather_app/static/js/runtime-main.6a5e8f53.js"
+  }
+]);
